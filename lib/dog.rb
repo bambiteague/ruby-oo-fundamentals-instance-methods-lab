f00acaf2f04dcf08513fe9a870e93f18e53980ef
bambi teague <teeegsa@gmail.com>
2020-12-15 11:45:06 -0600

    commit tree 993bae912880f54fd75dcae8f75ce86dec74b4dc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,29 @@
+# class Dog
+#    def bark
+#     puts "Woof!"
+#    end
+#    def sit
+#     puts "The Dog is sitting"
+#    end
+
+# end
+
+# fido = Dog.new
+# fido.bark
+# fido.sit
+
+
+# retyping second time around for practice
+
 class Dog
+
    def bark
-    puts "Woof!"
-   end
-   def sit
-    puts "The Dog is sitting"
+      puts "Woof!"
    end
 
+   def sit
+      puts "The Dog is sitting"
+   end
 end
 
 fido = Dog.new
