bd5987e7a3ba1777df0bd53c7bb259c06d1e8cf4
bambi teague <teeegsa@gmail.com>
2020-12-15 11:46:53 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,26 @@
+# class Person
+
+#     def talk
+#         puts "Hello World!"
+#     end
+
+#     def walk
+#         puts "The Person is walking"
+#     end
+# end
+
+# ada = Person.new
+# ada.talk
+# ada.walk
+
+# retyping second time around for practice
+
 class Person
+
     def talk
         puts "Hello World!"
     end
+
     def walk
         puts "The Person is walking"
     end
